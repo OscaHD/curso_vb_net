ccfa3dad09cdebfcbb7679281917c035ed9be35a
OscarHD <o.hernandez@bbva.com>
2017-07-17 16:57:15 +0200

    carga 17072017

--- App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosExcel.vb
+++ App_WinForms_RRHH/App_WinForms_RRHH/Modelo/Empleados/EmpleadosExcel.vb
@@ -1,57 +1,25 @@
 ﻿
+Imports System.Data.OleDb
+
 Namespace Modelo
     Public Class EmpleadosExcel
-        Implements IPersistenciaEmpleados
-        Private _nombreFichero As String
-        Public Property NombreFichero As String Implements IPersistenciaEmpleados.nombreFichero
-            Get
-                Return _nombreFichero
-            End Get
-            Set(value As String)
-                If (value = "") Then
-                    Throw New Exception("No se ha establido el nombre del fichero")
-                Else
-                    _nombreFichero = value
-                End If
-            End Set
-        End Property
+        Inherits EmpleadosBaseOleDB
 
-        Public Function Importar(ByRef arrayEmpleados() As Empleado) As Boolean Implements IPersistenciaEmpleados.Importar
-            MessageBox.Show("Importar " & NombreFichero)
-            Throw New NotImplementedException()
+        Public Overrides Function Importar(ByRef arrayEmpleados() As Empleado) As Boolean
+
+            cadena_conexion = CrearCadenaConexion(PROVEEDOR_EXCEL, NombreFichero) & PROPIEDADES_EXCEL
+
+            consultaSQL = "SELECT * FROM [Hoja1$A2:E65535]"
+
+            Return MyBase.Importar(arrayEmpleados)
         End Function
-        Public Function Exportar(arrayEmpleados() As Empleado) As Boolean Implements IPersistenciaEmpleados.Exportar
 
-            Dim oExcel As Object
-            Dim oLibro As Object
-            Dim oHoja As Object
-            Try
-                ' Creamos un objeto cuyo contenido es una instancia de Excel
-                oExcel = CreateObject("Excel.Application")
-                ' Comenzar nuevo libro de Excel
-                oLibro = oExcel.Workbooks.Add
-                oHoja = oLibro.Worksheets(1)
+        Public Overrides Function Exportar(arrayEmpleados() As Empleado) As Boolean
+            cadena_conexion = CrearCadenaConexion(PROVEEDOR_EXCEL, NombreFichero) & PROPIEDADES_EXCEL
 
-                Dim ArrayDatos(arrayEmpleados.Count, 4) As String
-                Dim r As Integer
-                For r = 0 To arrayEmpleados.Count - 1
-                    ArrayDatos(r, 0) = arrayEmpleados(r).nombre
-                    ArrayDatos(r, 1) = arrayEmpleados(r).apellidos
-                    ArrayDatos(r, 2) = arrayEmpleados(r).genero
-                    ArrayDatos(r, 3) = arrayEmpleados(r).categoria
-                    ArrayDatos(r, 4) = arrayEmpleados(r).retribucionFija
-                Next
-                oHoja.Range("A1:E1").Value = New String() {"NOMBRE", "APELLIDOS", "GENERO", "CATEGORIA", "RETRIBUCIÓN FIJA"}
-                oHoja.Range("A2").Resize(arrayEmpleados.Length, 5).Value = ArrayDatos
+            consultaSQL = "INSERT INTO [Hoja1$] ( Nombre, Apellidos, Genero, Categoria, Retribucion_Fija ) VALUES (@nombre, @apellidos, @genero, @categoria, @retribucionFija )"
 
-                ' Guardar el libro y salir de Excel
-                oLibro.SaveAs(NombreFichero)
-                oExcel.Quit()
-            Catch ex As Exception
-                MessageBox.Show("Error: " + ex.ToString())
-                Return False
-            End Try
-            Return True
+            Return MyBase.Exportar(arrayEmpleados)
         End Function
 
     End Class
